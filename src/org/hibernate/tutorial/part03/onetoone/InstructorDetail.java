2fd0ec67b1292a44fbc12cb14d8ff76f20d1a2b1
tutkuince <tutku.ince@outlook.com>
2018-06-22 16:39:09 +0300

    update for bi-directional relation

--- src/org/hibernate/tutorial/part03/onetoone/InstructorDetail.java
+++ src/org/hibernate/tutorial/part03/onetoone/InstructorDetail.java
@@ -1,10 +1,12 @@
 package org.hibernate.tutorial.part03.onetoone;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -20,6 +22,10 @@ public class InstructorDetail {
 
 	@Column(name = "hobby")
 	private String hobby;
+
+	// Bi-directional 
+	@OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+	private Instructor instructor;
 
 	public InstructorDetail() {
 
@@ -53,6 +59,14 @@ public class InstructorDetail {
 
 	public void setHobby(String hobby) {
 		this.hobby = hobby;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
 	}
 
 	@Override
