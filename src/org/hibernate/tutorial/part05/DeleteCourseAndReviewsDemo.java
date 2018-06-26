f02ff34ee786ae164257050c1ec442ff9d04cafe
tutkuince <tutku.ince@outlook.com>
2018-06-26 12:52:45 +0300

    get courses and reviews, delete courses and reviews samples

---
+++ src/org/hibernate/tutorial/part05/DeleteCourseAndReviewsDemo.java
@@ -0,0 +1,40 @@
+package org.hibernate.tutorial.part05;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.tutorial.util.HibernateUtil;
+
+public class DeleteCourseAndReviewsDemo {
+	public static void main(String[] args) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+		Session session = sessionFactory.getCurrentSession();
+
+		try {
+			// Start transaction
+			session.beginTransaction();
+			
+			// get the course
+			int id = 1;
+			Course course = session.get(Course.class, id);
+			
+			// print the course
+			System.out.println("Deleting the course . . .");
+			System.out.println("Course: " + course);
+			
+			// print the course reviews
+			course.getReviews().forEach(System.out::println);
+			
+			// delete the course
+			session.remove(course);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			HibernateUtil.shutdown();
+		}
+	}
+}
